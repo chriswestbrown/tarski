5ce8413df27b3eede3e56fe6934553bfc7b99a60
Fernando <fernvale25@gmail.com>
2018-07-05 21:20:19 -0400

    Update for mksysdep.sh and Makefile

--- interpreter/src/mksysdep.sh
+++ interpreter/src/mksysdep.sh
@@ -4,12 +4,14 @@ if [ ! -f tarskisysdep.h ]; then
 {
 echo '#ifndef _TARSKI_SYSDEP_H_'
 echo '#define _TARSKI_SYSDEP_H_'
+echo '#include "string.h"'
 echo
 echo 'namespace tarski {'
 echo 'static const char * pathToQepcad = "'$(which qepcad | tr -d '\n')'";'
 echo 'static const char * pathToMaple = "'$(which maple | tr -d '\n')'";'
 echo 'static const char * pathToSingular = "'$(which Singular | tr -d '\n')'";'
 echo
+echo 'if (strcmp(pathToQepcad, "") == 0) throw TarskiException("Invalid location for QEPCAD")'
 echo '} // end namespace tarski'
 echo '#endif'
 } > tarskisysdep.h
