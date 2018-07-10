5b75eefc6de917036a0817bfaa733a25ec9e75cb
Christopher W. Brown <wcbrown@usna.edu>
2018-07-10 14:42:01 -0400

    tweaked build script

--- tarInstall.sh
+++ tarInstall.sh
@@ -4,7 +4,7 @@ trap "exit 1" TERM
 export TOP_PID=$$
 
 function check(){
-    $1 > /dev/null
+    $1 # > /dev/null
     if [ $? -ne 0 ]
     then
         echo "Failed on" $1
@@ -12,7 +12,12 @@ function check(){
     fi
 }
 
-cd $saclib
+tarskiRoot=$(pwd)
+
+### SACLIB
+saclibRoot="$tarskiRoot/saclib2.2.7"
+export saclib=$saclibRoot
+pushd $saclib
 echo "Making SACLIB..."
 check bin/sconf
 check bin/mkproto
@@ -20,17 +25,46 @@ check bin/mkmake
 check "bin/mklib all"
 check
 echo "Saclib done"
-cd $qe
+popd
+
+### QEPCAD
+qepcadRoot="$tarskiRoot/qesource"
+export qe=$qepcadRoot
+pushd $qe
 echo "Making QEPCAD..."
 check make
 echo "QEPCAD done"
-cd $TMROOT/core
+popd
+
+### HACKED MINISAT
+minisatRoot="$tarskiRoot/minisat"
+export TMROOT=$minisatRoot
+pushd $TMROOT/core
 echo "Making Minisat..."
 check "make libr"
 echo "Minisat Done"
-cd ../../interpreter
+popd
+
+### TARSKI
+pushd interpreter
 echo "Making tarski..."
 check make
-echo "Tarski done. If you did not add $qe/bin to your path variable, add it now and call make in tarski/interpreter to properly set the location of QEPCAD."
+popd
+
+### FINAL MESSAGE
+echo -e "\nTarski done!"
+echo -e "######################################################"
+echo -e "IMPORTANT!!!  PLEASE READ BELOW!!!"
+echo -e "######################################################"
+echo -e "There are several environment variables that should be"
+echo -e "set in order to use or recompile Tarski.  So it is"
+echo -e "strongly recommended that the lines:\n"
+echo -e "export \$saclib=$saclibRoot"
+echo -e "export \$qe=$qepcadRoot\n"
+echo -e "are added to your .profile (or .bash_profile, depending"
+echo -e "on which you use) or whichever the equivalent file is on"
+echo -e "your system."
+echo -e "######################################################"
+
 
 
