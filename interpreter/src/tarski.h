2e16a504f97fb6c8736ed9126d3e0b696ebbf683
chriswestbrown <wcbrown@usna.edu>
2024-07-26 13:01:06 -0400

    Bumped tarski version to 1.40 reflecting addition of support for _root_ expressions in input for qepcad-api-call and qepcad-sat along with level-1 normalization support.

--- interpreter/src/tarski.h
+++ interpreter/src/tarski.h
@@ -5,8 +5,8 @@
 
 namespace tarski {
 
-static const char * tarskiVersion     = "1.39";
-static const char * tarskiVersionDate = "Wed Jul 24 07:15:32 PM EDT 2024";
+static const char * tarskiVersion     = "1.40";
+static const char * tarskiVersionDate = "Fri Jul 26 12:55:38 PM EDT 2024";
 
 /*******************************************************************
  * TarskiException - This is the basic exception class for all
